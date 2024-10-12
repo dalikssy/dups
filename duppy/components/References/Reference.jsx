7c0474d10bd76044c013779cbb13a917b1f5bb68
dalikssy <161870225+dalikssy@users.noreply.github.com>
2024-10-12 15:24:27 +0200

    Update Reference.jsx

--- duppy/components/References/Reference.jsx
+++ duppy/components/References/Reference.jsx
@@ -2,17 +2,21 @@ import Image from "next/image";
 import styles from "./References.module.css";
 
 export default function Reference({ refer, language, refNumber, index }) {
-  const { name, description, img, } = refer;
+  const { name, description, img, link } = refer;
 
   return (
-      <li className={`${styles.reference_profile} ${refNumber === index ? styles.active : styles.unactive}`}>
-        <div className={styles.img}>
-          <Image src={`/referals/${img}`} width={73} height={73} alt={name} />
-        </div>
-        <div>
-          <h4>{name}</h4>
-          <p>{description[language]}</p>
-        </div>
-      </li>
+    <li
+      className={`${styles.reference_profile} ${
+        refNumber === index ? styles.active : styles.unactive
+      }`}
+    >
+      <a className={styles.img} href={link} target="_blank">
+        <Image src={`/referals/${img}`} width={78} height={78} alt={name} />
+      </a>
+      <div>
+        <h4>{name}</h4>
+        <p>{description[language]}</p>
+      </div>
+    </li>
   );
 }
